6a9574da9fa98b1aef4398dca7e994827afc116e
satine fouque <satine.fouque@gmail.com>
2021-08-02 16:41:57 +0400

    methode create, new, edit, update ainsi que les validates de island créé

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,4 +1,9 @@
 class Island < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
