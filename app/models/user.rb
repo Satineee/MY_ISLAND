1d4c1353abf1dbd6d4bda745a72b341097266005
ugo lahaye <lahaye.ugo@hotmail.fr>
2021-08-02 16:44:18 +0400

    user controller for NEW CREATE and USER_ISLANDS

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :email, presence: true
 end
