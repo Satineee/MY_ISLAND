28df4d68e2ae6f2fc459cee0e73ef9df1bb92b84
ugo lahaye <lahaye.ugo@hotmail.fr>
2021-08-03 12:19:23 +0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+3.times{
+  user = User.new(email: Faker::Internet.email, password: 'azerty')
+  user.save
+    10.times{
+	  island = Island.new(name: Faker::Movies::LordOfTheRings.location, location: Faker::Address.country, price: rand(1099..16000), description: Faker::ChuckNorris.fact)
+      island.user = user
+      island.save
+  }
+}
