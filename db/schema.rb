ad2ec7411d9acb90feb622efdd7a73a09dc21af4
Dushyant Ramful <dushyantramful@gmail.com>
2021-08-04 14:15:26 +0400

    schema timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_064427) do
-# ActiveRecord::Schema.define(version: 2021_08_04_074657) do
+ActiveRecord::Schema.define(version: 2021_08_04_074657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
