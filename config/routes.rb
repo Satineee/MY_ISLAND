63f2feede726e66efbb91200299641e1d352dd71
satine fouque <satine.fouque@gmail.com>
2021-08-05 13:02:13 +0400

    delete reparé

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   authenticate :user do
-    resources :islands, only: [:new, :create, :edit, :update, :delete] do
+    resources :islands, only: [:new, :create, :edit, :update, :destroy] do
       resources :bookings, only: [:index, :show, :new, :create]
     end
   end
